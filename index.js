d1edee793e9af09ceb3665b115acd9ef15dc5d9b
Blake A Long <blake.a.long@gmail.com>
2019-02-27 18:07:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,4 +22,7 @@ function appendKitten(name) {
 function prependKitten(name) {
   return [name, ...kittens];
 }
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1);
+}
 // Add your functions and code here
